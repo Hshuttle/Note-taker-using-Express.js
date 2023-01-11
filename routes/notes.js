25353f77d2ac42404a3155edb28e561268956a87
Hshuttle <huntershutle@gmail.com>
2023-01-10 21:37:43 -0500

    updated notes.js to fix bug and finished readme

--- routes/notes.js
+++ routes/notes.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 const util = require("util");
 const readfile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile)
-const {v4:uuidv4}=require("uuid")
+const emptyArray = []
 
 function queryNotes() {
   return readfile("db/db.json", "utf-8").then((studentnotes) =>
-    [].concat(JSON.parse(studentnotes))
+    emptyArray.concat(JSON.parse(studentnotes))
   );
 }
 
@@ -17,9 +17,9 @@ router.get("/notes", (req, res) => {
 
 router.post("/notes", (req, res) => {
   queryNotes().then((notes) => {
-    const updatedArray = [...notes, {title:req.body.title, text:req.body.text, id:uuidv4()}]
-    console.log(updatedArray)
-    writeFile("db/db.json", JSON.stringify(updatedArray)).then(() => res.json({msg:"ok"})).catch(err => res.json(err))
+    const newArray = [...notes, {title:req.body.title, text:req.body.text, id:1}]
+    console.log(newArray)
+    writeFile("db/db.json", JSON.stringify(newArray)).then(() => res.json({msg:"ok"}))
   });
 });
 
